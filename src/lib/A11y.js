f6ae509d4792d963b37b8ccf1632dd5b1de594ae
Josh <zylo.codes@gmail.com>
2022-02-18 13:30:57 -0600

    A11y.toggleHighlightLinks()

--- src/lib/A11y.js
+++ src/lib/A11y.js
@@ -3,6 +3,8 @@ import styles from '../css/styles.css'
 
 class A11y {
   els = {}
+  menuOpen = false
+  linksHighlighted = false
   
   constructor() {
     this.init()
@@ -11,13 +13,16 @@ class A11y {
   init() {
     const menuId = `${PACKAGE_SLUG}-nav`
     const toggleButtonId = `${PACKAGE_SLUG}-toggle-button`
+    const highlightLinksButtonId = `${PACKAGE_SLUG}-highlight-links-button`
+
     this.els.styles = document.createElement('style')
     this.els.styles.innerText =  styles
+
     this.els.widget = document.createElement('div')
     this.els.widget.id = `${PACKAGE_SLUG}`
     this.els.widget.innerHTML = `
       <nav id="${menuId}" style="display: none;">
-        <button>Font Size</button>
+        <button id="${PACKAGE_SLUG}-highlight-links-button">Highlight Links</button>
         <button>Contrast</button>
         <button>Font Size</button>
         <button>Contrast</button>
@@ -27,8 +32,13 @@ class A11y {
       <button id="${toggleButtonId}">Toggle A11y</button>
     `
     this.els.menu = this.els.widget.querySelector(`#${menuId}`)
+
     this.els.toggleButton = this.els.widget.querySelector(`#${toggleButtonId}`)
+    this.els.highlightLinksButton = this.els.widget.querySelector(`#${highlightLinksButtonId}`)
+
     this.els.toggleButton.addEventListener('click', () => this.toggleMenu())
+    this.els.highlightLinksButton.addEventListener('click', () => this.toggleHighlightLinks())
+
     document.head.appendChild(this.els.styles)
     document.body.appendChild(this.els.widget)
   }
@@ -36,6 +46,15 @@ class A11y {
   toggleMenu = bool => {
     this.menuOpen = typeof bool !== 'undefined' && bool || !this.menuOpen
     this.els.menu.style.display = this.menuOpen ? 'block' : 'none'
+  }
+
+  toggleHighlightLinks() {
+    this.linksHighlighted = !this.linksHighlighted
+    for (const a of document.querySelectorAll('a')) {
+      this.linksHighlighted
+        ? a.classList.add('highlight')
+        : a.classList.remove('highlight')
+    }
   }
 }
 
